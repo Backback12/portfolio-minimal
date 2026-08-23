6a670760f55a027dadc05ee02aba7e5c96ae6e35
Connor Pagtakhan <cepagtakhan@gmail.com>
2026-08-23 00:07:23 -0500

    Updated packages for better markdown image support

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,19 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+import { unified } from '@astrojs/markdown-remark';
+import remarkAttributes from 'remark-attributes';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://connors-stuff.dev',
   base: '/',
   vite: {
     plugins: [tailwindcss()]
+  },
+  markdown: {
+    processor: unified({
+      remarkPlugins: [remarkAttributes],
+    }),
   },
   devToolbar: {
     enabled: false
