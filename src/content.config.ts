7f62d243ea26e66e15c44f74fe55c2fad8b94d78
Connor Pagtakhan <cepagtakhan@gmail.com>
2026-09-06 02:03:39 -0500

    Update events collection to be in its own dir

--- src/content.config.ts
+++ src/content.config.ts
@@ -31,7 +31,23 @@ const blogCollection = defineCollection({
     }),
 });
 
+const eventsCollection = defineCollection({
+  loader: glob({
+    pattern: '**/*.mdx',
+    base: './src/content/events',
+  }),
+  schema: ({ image }) =>
+    z.object({
+      event: z.string(),
+      date: z.coerce.date(),
+      location: z.string(),
+      image: z.string(),
+      description: z.string(),
+    }),
+});
+
 export const collections = {
   projects: projectsCollection,
   blog: blogCollection,
+  events: eventsCollection,
 };
